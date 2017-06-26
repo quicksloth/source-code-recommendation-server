69788bfb5acccb63994927503e1ba24e61e8ad79
pamepeixinho <pameiupip@gmail.com>
2017-06-26 09:32:48 -0300

    starting logic of input

--- ModulesConcepts/ComplexNetwork.py
+++ ModulesConcepts/ComplexNetwork.py
@@ -6,12 +6,17 @@ class ComplexNetwork(object):
     Attributes:
         textual_train_base: A array of strings to train complex network based on co-occurrence
     """
-    complex_network_file = "data_complex_network.npy"
+    complex_network_file = "adjacency_list_complex_network.npy"
     adjacency_list = dict([])
 
     def __init__(self, textual_train_base):
-        # TODO if textual_train_base is empty : load save complex network by file *data_complex_network.npy*
-        self.textual_train_base = textual_train_base
+        # TODO if textual_train_base is empty : load save complex network by file *adjacency_list_complex_network.npy*
+        if textual_train_base:
+            self.textual_train_base = textual_train_base
+            self.train_network
+            self.__save_complex_network()
+        else:
+            self.__load_complex_network()
 
     def __load_complex_network(self):
         """Returns the ComplexNetwork data loaded by a file"""
@@ -21,9 +26,26 @@ class ComplexNetwork(object):
         """Save ComplexNetwork data into a file and returns the data"""
         return self.complex_network_file
 
+    @property
     def train_network(self):
-        """Create structure and train ComplexNetwork based on textual_train_base"""
-        return self.textual_train_base
+        """Create structure and train ComplexNetwork based on textual_train_base
+        Returns trained """
+
+        for doc in self.textual_train_base:
+            print doc
+            doc_splitted = doc.split()
+            for idx, word in enumerate(doc_splitted):
+                if idx-1 == len(doc_splitted):
+                    break
+                print word
+                next_word = doc_splitted[idx + 1]
+                print "next word = %s" % next_word
+                self.adjacency_list[word, next_word] = self.adjacency_list[word, next_word]+1\
+                    if self.adjacency_list[word, next_word]\
+                    else 0
+
+        print self.adjacency_list
+        return self.adjacency_list
 
     def get_contextual_distance(self, one_word, second_word):
         """Create structure and train ComplexNetwork based on textual_train_base"""
@@ -31,3 +53,8 @@ class ComplexNetwork(object):
         print(one_word)
         print(second_word)
         return 0.3
+
+t1 = 'Lorem ipsum dolor sit amet Nullam metus.'
+t2 = 'Consectetur adipiscing elit.'
+textual = [t1, t2]
+ComplexNetwork(textual_train_base=textual)
