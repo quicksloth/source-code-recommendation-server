2711f6e9443b6ba56140e8346e7a701e3764d818
pamepeixinho <pameiupip@gmail.com>
2017-08-27 20:48:05 -0300

    testing with curl and adding issue comment

--- src/Controllers/EvaluatorController.py
+++ src/Controllers/EvaluatorController.py
@@ -1,6 +1,9 @@
 import sys
 import os
 
+# TODO: try to find another way to solve this problem
+# issue 12 https://github.com/quicksloth/source-code-recommendation-server/issues/11
+# Necessary to import modules in the same level
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
