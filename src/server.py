8ea785048110155e8cee982c36bc6a46f27e6bf8
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2017-09-09 12:02:55 -0300

    web socket working, trying to emit out of server file context

--- src/server.py
+++ src/server.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, json
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit, disconnect
 import requests
 
 from Controllers.EvaluatorController import EvaluatorController
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='')
 app.config['SECRET_KEY'] = '@server-secret'
 socketio = SocketIO(app)
 
+clients = []
 
 @app.route('/')
 def hello_world():
@@ -39,12 +40,75 @@ def train_network():
     EvaluatorController().train_network(train_database=train_base)
     return json.dumps({'success': True})
 
+# TRY SOCKET -----------------------------
+# Object that represents a socket connection
+class Socket:
+    def __init__(self, sid):
+        self.sid = sid
+        self.connected = True
+        print('init Socket')
+
+    # Emits data to a socket's unique room
+    def emit(self, event, data):
+        print('going to emit in ', self.sid)
+        with app.app_context():
+            emit(event, data, room=self.sid)
+
+
+@socketio.on('connect')
+def foo():
+    print('connect')
+    # EvaluatorController
+    # clients.append(Socket(request.sid))
+    print(request.sid)
+    # print(clients)
+    # clients[0].emit('message', {'show': True})
+    EvaluatorController().init_get_recommendation_code_fake(request_id=request.sid)
+
+# @socketio.on('disconnect')
+# def disconnect():
+#     print("%s disconnected" % (request.namespace.socket.sessid))
+#     clients.remove(request.namespace)
+
+
+def hello_to_random_client(request_id):
+    import random
+    from datetime import datetime
+    print('hello_to_random_client')
+    Socket(request_id).emit('message', {'show': True})
+    # print(clients)
+    # if clients:
+    #     clients[0].emit('message', { 'show': True})
+    # k = random.randint(0, len(clients) - 1)
+    # print("Saying hello to %s" % (clients[k].socket.sessid))
+    # clients[k].emit('message', "Hello at %s" % (datetime.now()))
+
 
 @socketio.on('json')
 def handle_json(json):
+    print('handle_json')
     print('received json: ' + str(json))
 # https://flask-socketio.readthedocs.io/en/latest/
 
+
+@socketio.on('try')
+def receive():
+    # print('sendMessage')
+    emit('teste', {'data': 42})
+    disconnect()
+
+
+@socketio.on('serverMessage')
+def handle_server_message(message):
+    print('the message is %s', message)
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('my event')
+    print('received json: ' + str(json))
+
+# TRY SOCKET -----------------------------
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=6060)
-    socketio.run(app)
+    socketio.run(app, debug=True)
---
+++ src/testMessageWebSocket.py
@@ -0,0 +1,3 @@
+import server
+
+server.hello_to_random_client('d0bc5e4debbd4dd681f336b977b09946')
