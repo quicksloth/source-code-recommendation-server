223be66a48bfa1f9d063ed4a861a63c3e53219e1
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2017-09-29 22:23:54 -0300

    improving

--- src/Models/DTO/Crawler/CrawlerCodeDTO.py
+++ src/Models/DTO/Crawler/CrawlerCodeDTO.py
@@ -5,11 +5,11 @@ class CrawlerCodeDTO(object):
 
     def __init__(self, id, libs, comments, variable_names, function_names, class_name, lines_number, code):
         self.id = id
-        self.libs = libs if libs else []
+        self.libs = libs if len(libs) > 0 else []
         self.comments = comments if len(comments) > 0 else []
-        self.variable_names = variable_names if variable_names else []
-        self.function_names = function_names if function_names else []
-        self.class_name = class_name if class_name else []
+        self.variable_names = variable_names if len(variable_names) > 0 else []
+        self.function_names = function_names if len(function_names) > 0 else []
+        self.class_name = class_name if len(class_name) > 0 else []
         self.lines_number = lines_number if lines_number else 1
         self.score = 0.0
         self.code = code
