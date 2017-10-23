4de0bb858d68c7f21941290b2fd353ce516d298e
pamepeixinho <pameiupip@gmail.com>
2017-10-23 19:25:50 -0200

    use private methods

--- src/Modules/NlpModule.py
+++ src/Modules/NlpModule.py
@@ -18,7 +18,7 @@ class NlpModule(AbstractModule):
         self.clusters = self.complex_network.get_clusters_content()
         self.clustersCount = len(self.clusters)
 
-    def populate_histogram(self, histogram, doc):
+    def __populate_histogram(self, histogram, doc):
         # Populate histogram
         words = doc.split(" ")
         for word in words:
@@ -26,7 +26,11 @@ class NlpModule(AbstractModule):
                 cluster_array = cluster.split("\t")
                 if word in cluster_array:
                     histogram[i] += 1
-        # Normalize Histogram
+        # Return normalized histogram
+        return self.__normalize_histogram(histogram)
+
+    @staticmethod
+    def __normalize_histogram(histogram):
         histogram_sum = 0
         for value in histogram:
             histogram_sum += value
@@ -35,45 +39,46 @@ class NlpModule(AbstractModule):
                 histogram[i] = value / histogram_sum
         return histogram
 
-    def evaluate_docs_distance(self, doc1, doc2):
-        histogram1 = numpy.array(self.populate_histogram([0] * self.clustersCount, doc1))
-        histogram2 = numpy.array(self.populate_histogram([0] * self.clustersCount, doc2))
+    def __evaluate_docs_distance(self, doc1, doc2):
+        histogram1 = numpy.array(self.__populate_histogram([0] * self.clustersCount, doc1))
+        histogram2 = numpy.array(self.__populate_histogram([0] * self.clustersCount, doc2))
         return numpy.linalg.norm(histogram1 - histogram2)
 
-    def evaluate_query_vs_doc(self, query, doc):
-        score = 1 - self.evaluate_docs_distance(query, doc)
+    def __evaluate_query_vs_doc(self, query, doc):
+        score = 1 - self.__evaluate_docs_distance(query, doc)
         return self.internal_weights[0] * score
 
-    def evaluate_query_vs_comments(self, query, user_comments):
+    def __evaluate_query_vs_comments(self, query, user_comments):
         grouped_comments = " ".join(user_comments)
-        score = 1 - self.evaluate_docs_distance(query, grouped_comments)
+        score = 1 - self.__evaluate_docs_distance(query, grouped_comments)
         return self.internal_weights[1] * score
 
-    def evaluate_comments_vs_comments(self, user_comments, code_comments):
+    def __evaluate_comments_vs_comments(self, user_comments, code_comments):
         grouped_user_comments = " ".join(user_comments)
         grouped_code_comments = " ".join(code_comments)
-        score = 1 - self.evaluate_docs_distance(grouped_user_comments, grouped_code_comments)
+        score = 1 - self.__evaluate_docs_distance(grouped_user_comments, grouped_code_comments)
         return self.internal_weights[2] * score
 
-    def evaluate_comments_vs_doc(self, user_comments, doc):
+    def __evaluate_comments_vs_doc(self, user_comments, doc):
         grouped_user_comments = " ".join(user_comments)
-        score = 1 - self.evaluate_docs_distance(grouped_user_comments, doc)
+        score = 1 - self.__evaluate_docs_distance(grouped_user_comments, doc)
         return self.internal_weights[3] * score
 
     def evaluate_code(self, input_bus_vo, search_result_id, code_id):
         score = 0
         # Search Query vs Doc
-        score += self.evaluate_query_vs_doc(input_bus_vo.user.query,
-                                            input_bus_vo.searched_codes[search_result_id].documentation)
+        score += self.__evaluate_query_vs_doc(input_bus_vo.user.query,
+                                              input_bus_vo.searched_codes[search_result_id].documentation)
         # Search Query vs Comments
-        score += self.evaluate_query_vs_comments(input_bus_vo.user.query,
-                                                 input_bus_vo.searched_codes[search_result_id].codes[code_id].comments)
+        score += self.__evaluate_query_vs_comments(input_bus_vo.user.query,
+                                                   input_bus_vo.searched_codes[search_result_id].codes[
+                                                       code_id].comments)
         # User Comments vs Doc
-        score += self.evaluate_comments_vs_doc(input_bus_vo.user.comments,
-                                               input_bus_vo.searched_codes[search_result_id].documentation)
+        score += self.__evaluate_comments_vs_doc(input_bus_vo.user.comments,
+                                                 input_bus_vo.searched_codes[search_result_id].documentation)
         # User Comments vs Comments
-        score += self.evaluate_comments_vs_comments(input_bus_vo.user.comments,
-                                                    input_bus_vo.searched_codes[search_result_id].codes[
-                                                        code_id].comments)
+        score += self.__evaluate_comments_vs_comments(input_bus_vo.user.comments,
+                                                      input_bus_vo.searched_codes[search_result_id].codes[
+                                                          code_id].comments)
 
         return self.weight * score
