a611629336e46afdf54301355f15e0829d73ba55
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2017-10-24 16:08:22 -0200

    Refactor NLP (#31)
    * test nlp fake crawler
    
    * first take in refactor
    
    * adding more checks
    
    * evaluating
    
    * use default mcl

--- src/Modules/NlpModule.py
+++ src/Modules/NlpModule.py
@@ -1,6 +1,5 @@
 from Modules.AbstractModule import AbstractModule
 from Modules.Concepts.ComplexNetwork import ComplexNetwork
-import numpy
 
 
 class NlpModule(AbstractModule):
@@ -15,53 +14,56 @@ class NlpModule(AbstractModule):
             internal_weights = [0.25, 0.25, 0.25, 0.25]
         AbstractModule.__init__(self, internal_weights, weight)
         self.complex_network = ComplexNetwork()
-        self.clusters = self.complex_network.get_clusters_content()
-        self.clustersCount = len(self.clusters)
+    #
+    # def __populate_histogram(self, histogram, doc):
+    #     # Populate histogram
+    #     words = doc.split(" ")
+    #     for word in words:
+    #         for i, cluster in enumerate(self.clusters):
+    #             cluster_array = cluster.split("\t")
+    #             if word in cluster_array:
+    #                 histogram[i] += 1
+    #     # Return normalized histogram
+    #     return self.__normalize_histogram(histogram)
+    #
+    # @staticmethod
+    # def __normalize_histogram(histogram):
+    #     histogram_sum = 0
+    #     for value in histogram:
+    #         histogram_sum += value
+    #     if histogram_sum > 0:
+    #         for i, value in enumerate(histogram):
+    #             histogram[i] = value / histogram_sum
+    #     return histogram
+    #
+    # def __evaluate_docs_distance(self, doc1, doc2):
+    #     print("doc1", doc1)
+    #     print("doc2", doc2)
+    #     histogram1 = numpy.array(self.__populate_histogram([0] * self.clustersCount, doc1))
+    #     histogram2 = numpy.array(self.__populate_histogram([0] * self.clustersCount, doc2))
+    #     return numpy.linalg.norm(histogram1 - histogram2)
 
-    def __populate_histogram(self, histogram, doc):
-        # Populate histogram
-        words = doc.split(" ")
-        for word in words:
-            for i, cluster in enumerate(self.clusters):
-                cluster_array = cluster.split("\t")
-                if word in cluster_array:
-                    histogram[i] += 1
-        # Return normalized histogram
-        return self.__normalize_histogram(histogram)
-
-    @staticmethod
-    def __normalize_histogram(histogram):
-        histogram_sum = 0
-        for value in histogram:
-            histogram_sum += value
-        if histogram_sum > 0:
-            for i, value in enumerate(histogram):
-                histogram[i] = value / histogram_sum
-        return histogram
-
-    def __evaluate_docs_distance(self, doc1, doc2):
-        histogram1 = numpy.array(self.__populate_histogram([0] * self.clustersCount, doc1))
-        histogram2 = numpy.array(self.__populate_histogram([0] * self.clustersCount, doc2))
-        return numpy.linalg.norm(histogram1 - histogram2)
+    def __get_score_by_doc_combination(self, first_doc, second_doc):
+        return 1 - self.complex_network.get_contextual_distance_between_docs(first_doc, second_doc)
 
     def __evaluate_query_vs_doc(self, query, doc):
-        score = 1 - self.__evaluate_docs_distance(query, doc)
+        score = self.__get_score_by_doc_combination(query, doc)
         return self.internal_weights[0] * score
 
     def __evaluate_query_vs_comments(self, query, user_comments):
         grouped_comments = " ".join(user_comments)
-        score = 1 - self.__evaluate_docs_distance(query, grouped_comments)
+        score = self.__get_score_by_doc_combination(query, grouped_comments)
         return self.internal_weights[1] * score
 
     def __evaluate_comments_vs_comments(self, user_comments, code_comments):
         grouped_user_comments = " ".join(user_comments)
         grouped_code_comments = " ".join(code_comments)
-        score = 1 - self.__evaluate_docs_distance(grouped_user_comments, grouped_code_comments)
+        score = self.__get_score_by_doc_combination(grouped_user_comments, grouped_code_comments)
         return self.internal_weights[2] * score
 
     def __evaluate_comments_vs_doc(self, user_comments, doc):
         grouped_user_comments = " ".join(user_comments)
-        score = 1 - self.__evaluate_docs_distance(grouped_user_comments, doc)
+        score = self.__get_score_by_doc_combination(grouped_user_comments, doc)
         return self.internal_weights[3] * score
 
     def evaluate_code(self, input_bus_vo, search_result_id, code_id):
