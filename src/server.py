18cd98e66ebd78acc1edeaf2c1064a02480402a6
pamepeixinho <pameiupip@gmail.com>
2017-11-10 17:11:13 -0200

    try

--- src/server.py
+++ src/server.py
@@ -103,7 +103,7 @@ def emit_code_recommendations(request_id, data):
 
 
 def run_server():
-    socketio.run(app, host='0.0.0.0', port=10443, threaded=True)
+    socketio.run(app=None, host='0.0.0.0', port='10443', async_mode='eventlet')
 
 
 if __name__ == "__main__":
