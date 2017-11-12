e6cce709411ab4ef43047357c06f31326eb4447e
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2017-11-12 13:44:47 -0200

    update crawler url (#43)

--- src/server.py
+++ src/server.py
@@ -53,7 +53,8 @@ def source_codes():
 
 
 def get_source_codes(data):
-    url = 'http://0.0.0.0:1111/crawl'
+    # url = 'http://0.0.0.0:1111/crawl'
+    url = 'https://quickslothsearchserver.herokuapp.com/crawl'
     headers = {'Content-Type': 'application/json'}
     print('going to request new version')
     requests.request(url=url, method='GET', data=data, headers=headers)
