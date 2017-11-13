de78aeb42a3871aa31d69746445e07be38590e76
pamepeixinho <pameiupip@gmail.com>
2017-11-13 13:17:54 -0200

    local

--- src/FileExamples/ExampleSort.py
+++ src/FileExamples/ExampleSort.py
@@ -30,7 +30,7 @@ class MyProjectObjectList(object):
         pfile = open((filename or self.file), 'wb+')
         pickle.dump(self.list, pfile)
 
-    # def __reading_file(self):
+    def __reading_file(self):
         # Return a file with list of lines
 
 
